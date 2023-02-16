a3b1faf22cfd5c3f656a0cccf3a115815c9b0044
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 04:29:13 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: 0
 */
 int main(void)
-{
-	printf("with proper gramer, but the outcome is a piece of art,\n");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
