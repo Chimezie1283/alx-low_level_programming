cb48acc547838bf0aaa280a8fdf7922a26464cd2
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 07:28:12 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-	write(stdout, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	59);
 	return (1);
 }
