5210527c9e3322e3a623b972252f1b6dbbf6af50
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 04:12:25 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- print a second line of code
+ *
+ * Return:Always 0
+*/
+int main(void)
+{
+	print("with proper gramer, but the outcome is a piece of art,");
+	return (0);
+}
