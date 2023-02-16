53ddd6c4e56d4634fce6acb4449fdfe21004b2b9
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 04:22:20 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main- print a c program using printf
+ * main - print a c program using printf
  *
- * Return:Always 0
+ * Return: 0
 */
 int main(void)
 {
-	printf("with proper gramer, but the outcome is a piece of art,\n");
+	printf("with proper gramer, but the outcome is a piece of art,\\n");
 	return (0);
 }
