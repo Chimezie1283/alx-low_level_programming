2bfa4d7b14505c6b9ead3a4d55158399d934fdd8
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 07:46:21 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - prints that piece of art is useful"
- * - Dora Korpar, 2015-10-19
+ * main - prints that piece of art is useful
  *
  * Return: Always 1
  */
 int main(void)
 {
-	write(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	59);
+	write("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
