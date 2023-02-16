daf1b21c1d8cff96829595fadb8b0fe06f000b81
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-16 05:34:31 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - print a c program using printf
+ * main - print a c code using printf
  *
- * Return: 0
-*/
+ * Return: Always 0
+ */
 int main(void)
-
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
