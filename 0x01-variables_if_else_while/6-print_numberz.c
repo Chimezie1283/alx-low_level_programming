b599812473a99fe3fcced135531a0e571e8a031a
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-17 03:35:40 -0800

    print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar("\n");
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
 	return (0);
 }
