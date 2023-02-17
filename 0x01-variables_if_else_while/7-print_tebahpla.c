5be84cf9a0250da95b3145b7922f35c51cdfb213
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-17 03:04:51 -0800

    print_tebalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch => 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
