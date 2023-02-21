648f02e1a3b6536aeb479439bd81cf1764ce48b3
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-21 02:57:54 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alph
+
+	for (alph = 'a'; alph <= 'z'; aplh++)
+		_putchar(alph);
+
+	_putchar('\n');
+
+}
