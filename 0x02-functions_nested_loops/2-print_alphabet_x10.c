2dc43f78c6e5c0942da60953ecf35ae7ff23acad
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-21 08:02:59 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+	char b;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (b = 'a'; c <= 'z'; c++)
+			_putchar(b);
+
+		_putchar('\n');
+	}
+}
