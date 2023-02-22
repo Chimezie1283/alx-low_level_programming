cf4479fe9edb32279729d6435653a8957d56712d
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-22 11:30:52 -0800

    3-print_remaining

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,20 +18,19 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-		else
+	else
+	{
+		if (month == 2 && day == 60)
 		{
-			if (month == 2 && day == 60)
-			{
-				printf("Invalid date: %02d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Days: %d\n", 365 - day);
 		}
-		}
+	}
 }	
