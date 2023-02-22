61a1460747d67cc6518edb18439534606dfe3325
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-21 20:47:29 -0800

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,9 +11,9 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for(n = 0; n <= 98; n++)
+		for (n = 0; n <= 98; n++)
 		{
-			if ( ==98)
+			if (n == 98)
 			{
 				printf("%d", n);
 				printf("\n");
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n = 0; n>= 98; n--)
+		for (n = 0; n >= 98; n--)
 		{
 			if (n == 98)
 			{
