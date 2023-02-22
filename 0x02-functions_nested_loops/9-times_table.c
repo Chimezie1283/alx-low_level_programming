abfd1a6559a42b45c9bf967bb0bdfbcbf930cef7
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-21 20:24:08 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int x, y, z, a, b;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				a = z % 10;
+				b = (z - a) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(b + '0');
+				_putchar(a + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
