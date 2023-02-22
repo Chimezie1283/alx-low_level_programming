c4fc43ab8f14c496581c3b9ebc727b5a70d28f3b
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-21 20:09:30 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,16 +11,16 @@ void jack_bauer(void)
 
 	for (m = 0; m <= 2; m++)
 	{
-		for (n = 0; n <= 9; b++)
+		for (n = 0; n <= 9; n++)
 		{
 			if ((m <= 1 && n <= 9) || (m <= 2 && n <= 3))
 			{
-				for (o = 0; 0 <= 5; 0++)
+				for (o = 0; o <= 5; o++)
 				{
 					for (p = 0; p <= 9; p++)
 					{
 						_putchar(m + '0');
-						_putchar(o + '0');
+						_putchar(n + '0');
 						_putchar(58);
 						_putchar(o + '0');
 						_putchar(p + '0');
