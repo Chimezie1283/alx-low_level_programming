1324d0d08c1ee6ee1b6013d977f8c942699d2a47
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-22 00:06:33 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n: The multiplication request
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int a, b, chi;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				chi = (a * b);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (chi < 10 && b != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((chi % 10) + '0');
+				}
+				else if (chi >= 10 && chi < 100)
+				{
+					_putchar(' ');
+					_putchar((chi / 10) + '0');
+					_putchar((chi % 10) + '0');
+				{
+				else if (chi >= 100 && b != 0)
+				{
+					_putchar((chi / 100) + '0');
+					_putchar((chi / 10) % 10 + '0');
+					_putchar((chi % 10) + '0');
+				}
+				else
+					_putchar((chi % 10) + '0');
+			}
+				_putchar('\n');
+			}
+			}
+		}
+	}
+}
