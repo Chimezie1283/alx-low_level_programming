5f9deee39911777966ef84fb738493cd81c837a6
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-22 09:58:42 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test function for possible or negative integer
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
