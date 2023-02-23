05648ac9663b88585bc6c0edae8fb70453682828
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-23 11:18:59 -0800

    #include main.h /** * _isupper - checks for uppercase character * @c: Variable text * Return: Always 0. */ int _isupper(int c) {
     if (c >= 'A' && c <= 'Z')
     {
     return (1);
     }
     else
    {
     return (0);
    }
    
    }
    
    '

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,17 @@
  * _isupper - checks for uppercase letters
  * @c: interger to check
  *
- * Return: 0 or 1
+ * Return: 1 if true, 0 if otherwise
  */
 int _isupper(int c)
 {
 
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
-
+	}
 	else
+	{
 		return (0);
+	}
 }
