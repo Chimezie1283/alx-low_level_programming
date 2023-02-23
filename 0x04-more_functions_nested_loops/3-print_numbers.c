b3e7cfa1f48405d43bf7fc0f8418b0ed91dcce5c
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-23 12:19:58 -0800

    3-print_numbe

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,15 @@
 /**
  * print_numbers - prints numbers from 0 to 9
  *
- * Return: 0 to 9
+ * Return: 0
  */
 void print_numbers(void)
 {
 	int m;
 
 	for (m = 0; m <= 9; m++)
-		_putchar(m + '0');
+
+		_putchar(m);
+
 	_putchar('\n');
 }
