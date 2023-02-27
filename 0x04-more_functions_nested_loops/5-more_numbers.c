4851860b91cf926af0b6b4f420ac048cd0cb4d58
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-02-27 05:47:03 -0800

    More-Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
-
 /**
  * more_numbers - print more numbers
  */
 void more_numbers(void)
 {
-        int i, j;
+	int i, j;
 
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
 
-        for (i = 1; i <= 10; i++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j >= 10)
-                                _putchar('1');
-
-                        _putchar (j % 10 + '0');
-                }
-                _putchar('\n');
-        }
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
