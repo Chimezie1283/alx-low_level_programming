f3bb5d711cc041b04d94badde81fb65617a7eae2
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-02 07:30:00 -0800

    103-infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,59 @@
 #include "main.h"
 
 /**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int k = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (k = 0; k < i; k++, i--)
+	{
+		temp = *(n + k);
+		*(n + k) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ *
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int k = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (k = 0; k < i; k++, k--)
+	{
+		temp = *(n + k);
+		*(n + k) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * *infinite_add - A function that adds 2 numbers
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
