aa4eaa9fd55880d73cc2cf0d8137be72745ae8bb
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-10 05:00:33 -0800

    updated File

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,20 +1,20 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _strchr - Entry point
  * @s: input
  * @c: input
+ *
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int h = 0;
 
-
-	for (; s[i] >= '\0'; i++)
+	for (; s[h] >= '\0'; h++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		if (s[h] == c)
+			return (&s[h]);
 	}
 	return (0);
 }
