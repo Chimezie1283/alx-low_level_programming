1464a1ad0bfc1c356d6197a5da22408f7b0760d7
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-13 02:02:27 -0700

    my 0x0A. C - argc, argv tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A function that prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+
+	for (m = 0; m < argc; m++)
+	{
+		printf("%s\n", argv[m]);
+	}
+
+	return (0);
+}
