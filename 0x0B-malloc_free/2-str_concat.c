799c569107129f4ef3610a90477ba71487276854
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-13 04:46:05 -0700

    2-str_concaT.C

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - A function that gets the end of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
+ * string_nconcat - A function that concatenate two strings
+ * @s1: First string
+ * @s2: String to add to end of s1
+ * @n: Amount of s2 to add to s1
  *
- * Return: concat of s1 and s2
+ * Return: pointer to new area in memory, NULL if it fails
  */
-char *str_concat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *conct;
-	int m, ci;
+	char *chi, *empty;
+	unsigned int m, men, p;
+	unsigned int size;
 
-
+	men = 0;
+	empty = "";
 	if (s1 == NULL)
-		s1 = "";
-
+		s1 = empty;
 	if (s2 == NULL)
-		s2 = "";
-
-		m = ci = 0;
-
-	while (s1[m] != '\0')
-		m++;
-
-	while (s2[ci] != '\0')
-		ci++;
-
-	conct = malloc(sizeof(char) * (m + ci + 1));
-
-	if (conct == NULL)
+		s2 = empty;
+	while (s1[men] != '\0')
+		men++;
+	size = (men + n) * sizeof(*chi);
+	chi = malloc(size + 1);
+	if (chi == NULL)
 		return (NULL);
-
-	m = ci = 0;
-
-	while (s1[m] != '\0')
+	m = 0;
+	while (m < size && s1[m] != '\0')
 	{
-		conct[m] = s1[m];
+		chi[m] = s1[m];
 		m++;
 	}
-	while (s2[ci] != '\0')
+	p = 0;
+	while (m < size && s2[p] != '\0')
 	{
-		conct[i] = s2[ci];
-		m++, ci++;
+		chi[m] = s2[p];
+		m++;
+		p++;
 	}
-	conct[m] = ('\0)';
-	return (conct);
+	chi[m] = '\0';
+	return (chi);
 }
