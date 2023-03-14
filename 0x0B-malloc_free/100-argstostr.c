924359b5afc1d025ffc97ea1dcc74da77cc0c5e0
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-14 05:04:41 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,58 +1,49 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
-/**
-		
- * argstostr - main entry
-		
- * @ac: int input
-		
- * @av: double pointer array
-		
- * Return: 0
-		
- */
-		
-char *argstostr(int ac, char **av)
-		
-{
-		
-	int i, n, r = 0, l = 0;
-		
-	char *str;
-		
 
-		
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ *
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int j, n, r = 0, l = 0;
+	char *str;
+
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
-		
 
-	for (i = 0; i < ac; i++)	
+
+	for (j = 0; j < ac; j++)
 	{
-		for (n = 0; av[i][n]; n++)
-			l++;		
+		for (n = 0; av[j][n]; n++)
+			l++;
 	}
 
 	l += ac;
 
 
 	str = malloc(sizeof(char) * l + 1);
-	
+
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)	
+	for (j = 0; j < ac; j++)
 	{
-	for (n = 0; av[i][n]; n++)	
+	for (n = 0; av[j][n]; n++)
 	{
-		str[r] = av[i][n];
-		r++;	
+		str[r] = av[j][n];
+		r++;
 	}
-	if (str[r] == '\0')	
+
+	if (str[r] == '\0')
 	{
-		str[r++] = '\n';	
+		str[r++] = '\n';
 	}
 	}
-	return (str);		
+	return (str);
 }
