ef73890d6a5d7c79978bd312b2873c4f9627f4c4
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-14 05:53:51 -0700

    101-strtow.c

---
+++ 0x0B-malloc_free/101-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
