b3b76e45a42e3689d1d449848501aa08a4f8ca9f
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-03-20 06:40:13 -0700

    task one on Structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+ /**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+dog_t;
+ /**
+ * dog_t - typedef for struct dog
+ */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
