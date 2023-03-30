4212676a947abfe4806af81d9fd040b9a2f55b2c
chimezie1283 <agbo.chimezie1283>
2023-03-30 01:38:34 -0700

    lists.h file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - The singly linked list
+ * @str: The string - (malloc'ed string)
+ * @len: The length of the string
+ * @next: Points to the next node
+ * Description: Singly linked list node structure for
+ * Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *follow;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
