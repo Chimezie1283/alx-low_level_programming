efd33f63ba4aa53a5df9168b9c7f4f9f57e8a463
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-04-04 03:31:11 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - A function that returns the number of elements in a linked lists
+ * listint_len - A function that returns the number
+ * of elements in a linked lists
  * @h: The linked list of type listint_t to traverse
  *
  * Return: The number of nodes
